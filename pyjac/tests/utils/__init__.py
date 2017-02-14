e3ffea0d779a00e4efea65ddc7901bd1f2c2aab8
arghdos <arghdos@gmail.com>
2017-02-14 17:36:31 -0500

    bugfix

--- pyjac/tests/utils/__init__.py
+++ pyjac/tests/utils/__init__.py
@@ -5,11 +5,11 @@ def __get_template(fname):
     with open(fname, 'r') as file:
         return Template(file.read())
 
-def get_import_source()
+def get_import_source():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     return __get_template(os.path.join(script_dir, 'test_import.py.in'))
 
-def get_read_ics_source()
+def get_read_ics_source():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     return (__get_template(os.path.join(script_dir, 'read_ic_wrapper.pyx.in')),
         __get_template(os.path.join(script_dir, 'read_ic_setup.py.in')))
