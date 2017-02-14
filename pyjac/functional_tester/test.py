686d311260265b0cfc2486f2a9bf65777f5588c6
arghdos <arghdos@gmail.com>
2017-02-14 17:39:42 -0500

    more imports

--- pyjac/functional_tester/test.py
+++ pyjac/functional_tester/test.py
@@ -40,7 +40,7 @@ from ..core.create_jacobian import create_jacobian
 from ..pywrap.pywrap_gen import generate_wrapper
 
 from ..tests.utils import data_bin_writer as dbw
-from ..tests.utils import test_matrix as tm
+from ..tests.utils import get_test_matrix as tm
 from ..tests import utils as test_utils
 
 
