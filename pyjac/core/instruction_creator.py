f7e2c3e51f9c8d598c90c031b2083e4559a73631
arghdos <arghdos@gmail.com>
2017-07-26 13:49:42 -0400

    add instruction creator to simplify masked update instructions

---
+++ pyjac/core/instruction_creator.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Contains various utility classes for creating update instructions
+(e.g., handling updating rates of progress w/ reverse ROP, pressure mod term,
+etc.)
+"""
+
+import logging
+import inspect
+from string import Template
+
+
+def get_update_instruction(mapstore, mask_arr, base_update_insn):
+    """
+    Handles updating a value by a possibly specified (masked value),
+    for example this can be used to generate an instruction (or set thereof)
+    to update the net rate of progress by the reverse or pressure modification
+    term
+
+    Parameters
+    ----------
+    mapstore: :class:`array_creator.MapStore`
+        The base mapstore used in creation of this kernel
+    mask_arr: :class:`array_creator.creator`
+        The array to use as a mask to determine whether the :param:`base_value`
+        should be updated
+    base_update_insn: str
+        The update instruction to use as a base.  This may be surrounded by
+        and if statement or possibly discarded altogether depending on the
+        :param:`mask_arr` and :param:`mapstore`
+
+    Returns
+    -------
+    update_insn: str
+        The update instruction string(s), possibly empty if no update is
+        required
+    """
+
+    if not mapstore.is_finalized:
+        _, _, line_number, function_name, _, _ = inspect.stack()[1]
+        logging.warn('Call to get_update_instruction() from {0}:{1}'
+                     ' used non-finalized mapstore, finalizing now...'.format(
+                         function_name, line_number))
+
+        mapstore.finalize()
+
+    if not mask_arr:
+        return ''
+
+    assert mask_arr in mapstore.domain_to_nodes, (
+        'Cannot create update instruction - mask array '
+        ' {} not in mapstore domains'.format(
+            mask_arr.name))
+
+    mask = mapstore.domain_to_nodes[mask_arr]
+    mask = mask if mask.parent in mapstore.transformed_domains \
+        else None
+    if mask:
+        mask_iname = mask.iname
+        return Template(
+            """
+    if ${mask_iname} >= 0
+        ${base_update_insn}
+    end
+    """).safe_substitute(**locals())
+
+    return base_update_insn
