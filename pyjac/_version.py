fd868651c31a234de6bb759f320651689c241e7a
Nick <nicholas.curtis@uconn.edu>
2017-10-25 09:56:13 -0400

    update version

--- pyjac/_version.py
+++ pyjac/_version.py
@@ -1,4 +1,2 @@
-__version_info__ = (1, 0, 6)
+__version_info__ = (2, 0, 0, 'beta')
 __version__ = '.'.join(map(str, __version_info__[:3]))
-if len(__version_info__) == 4:
-    __version__ += __version_info__[-1]
