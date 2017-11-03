be60158753ac0bfdb1a4adacbd5b3f4f9f7b9c18
Nick <nicholas.curtis@uconn.edu>
2017-11-03 18:16:20 -0400

    cleanup dbw

--- pyjac/tests/test_utils/data_bin_writer.py
+++ pyjac/tests/test_utils/data_bin_writer.py
@@ -2,11 +2,13 @@ import numpy as np
 import os
 from argparse import ArgumentParser
 
+
 def get_files(directory):
     return [os.path.join(directory, f) for f in os.listdir(directory)
-                if f.endswith('.npy') and
-                'pasr_out' in f
-                and os.path.isfile(os.path.join(directory, f))]
+            if f.endswith('.npy') and
+            'pasr_out' in f
+            and os.path.isfile(os.path.join(directory, f))]
+
 
 def load(npy_files, directory=None):
     if not npy_files and directory is not None:
@@ -23,16 +25,17 @@ def load(npy_files, directory=None):
         print(num_conditions, data.shape)
     return num_conditions, data
 
+
 def write(directory, cut=None):
     npy_files = get_files(directory)
     data = None
     filename = 'data.bin' if cut is None else 'data_eqremoved.bin'
     with open(os.path.join(directory, filename), 'wb') as file:
-        #load PaSR data for different pressures/conditions,
+        # load PaSR data for different pressures/conditions,
         # and save to binary C file
         num_conditions, data = load(npy_files)
         if num_conditions == 0:
-            print('No data found in folder {}, continuing...'.format(mech_name))
+            print('No data found in folder {}, continuing...'.format(directory))
             return 0
         if cut is not None:
             data = data[cut:, :]
@@ -40,8 +43,11 @@ def write(directory, cut=None):
         del data
     return num_conditions
 
+
 if __name__ == '__main__':
-    parser = ArgumentParser(description='data bin writer: Convenience script to generate binary files from .npy')
+    parser = ArgumentParser(
+        description='data bin writer: Convenience script to generate binary '
+                    'files from .npy')
     parser.add_argument('-d', '--directory',
                         type=str,
                         required=True,
@@ -50,13 +56,8 @@ if __name__ == '__main__':
                         type=int,
                         default=None,
                         required=False,
-                        help='The number of conditions to remove from the front of the database')
-    parser.add_argument('-o', '--order',
-                        type=str,
-                        default='F',
-                        choices=['C', F],
-                        required=False,
-                        help='The resulting order of the data binary')
+                        help='The number of conditions to remove from the front '
+                             'of the database')
     args = parser.parse_args()
-    main(os.path.realpath(os.path.dirname(args.directory)),
-        args.cut_off_front, args.order)
+    write(os.path.realpath(os.path.dirname(args.directory)),
+          args.cut_off_front)
