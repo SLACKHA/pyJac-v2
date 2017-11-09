d4a51e52929a587efbcd9c5aaa28f7a80cdc2188
Nick <nicholas.curtis@uconn.edu>
2017-11-09 11:15:40 -0500

    Add adept specs to site conf

--- site_conf_example.py
+++ site_conf_example.py
@@ -3,5 +3,11 @@ CL_LIBNAME = ['OpenCL']
 CL_VERSION = '1.2'
 CL_FLAGS = []
 CC_FLAGS = []
-#relies on OCL-ICD
+# relies on OCL-ICD
 CL_LIB_DIR = ['/usr/local/lib']
+# Adept is used in functional testing / unit testing for verification of Jacobian
+# entries versus autodifferentiated results -- note that a standard install
+# (i.e. to /usr/local/lib) probably won't need these specified
+ADEPT_INC_DIR = ['/path/to/adept/include']
+ADEPT_LIB_DIR = ['/path/to/adept/lib']
+ADEPT_LIBNAME = ['adept']
