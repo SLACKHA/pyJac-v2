2ba2480815f251aea5d554e912262e1312f0ab46
Nick <nicholas.curtis@uconn.edu>
2017-12-05 14:56:40 -0500

    add hook to specify max threads via test setup.py or env

--- test_setup.py
+++ test_setup.py
@@ -1,7 +1,7 @@
 # a configuration file for nose-testconfig that sets:
 # a) the test platforms file
-# and
 # b) the chemical mechanism to test
+# c) the maximum number of threads to test
 # note that both can be specified on the command line via ENV variables if desired
 # e.g. GAS=mymech.cti TEST_PLATFORMS=my_platform.yaml nosetests ...
 # or simply feel free to modify the below...
@@ -14,3 +14,4 @@ PLATFORM = 'test_platform.yaml'
 gas = os.path.join(home, 'pyjac', 'tests', 'test.cti')
 config['test_platform'] = os.path.join(home, PLATFORM)
 config['gas'] = gas
+# config['max_threads'] = None -- unused by default, uses # of hardware threads
