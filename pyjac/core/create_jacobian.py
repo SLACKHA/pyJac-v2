15d69792bb373d5d16b52ca18aae6845417aec9f
Nick <nicholas.curtis@uconn.edu>
2018-01-29 11:29:45 -0500

    move to unified calling function in utils that is used by all 'main's

--- pyjac/core/create_jacobian.py
+++ pyjac/core/create_jacobian.py
@@ -5297,26 +5297,4 @@ def create_jacobian(lang, mech_name=None, therm_name=None, gas=None,
 
 if __name__ == "__main__":
     utils.setup_logging()
-    args = utils.get_parser()
-
-    create_jacobian(lang=args.lang,
-                    mech_name=args.input,
-                    therm_name=args.thermo,
-                    vector_size=args.vector_size,
-                    wide=args.wide,
-                    deep=args.deep,
-                    unr=args.unroll,
-                    build_path=args.build_path,
-                    last_spec=args.last_species,
-                    platform=args.platform,
-                    data_order=args.data_order,
-                    rate_specialization=args.rate_specialization,
-                    split_rate_kernels=args.split_rate_kernels,
-                    split_rop_net_kernels=args.split_rop_net_kernels,
-                    conp=args.conp,
-                    use_atomics=args.use_atomics,
-                    jac_type=args.jac_type,
-                    jac_format=args.jac_format,
-                    skip_jac=False,
-                    mem_limits=args.memory_limits
-                    )
+    utils.create()
