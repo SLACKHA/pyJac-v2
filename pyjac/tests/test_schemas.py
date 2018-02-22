615cbffc61b439fe943ed0e245e8e5c512fc3cf0
Nick <nicholas.curtis@uconn.edu>
2018-02-22 16:06:57 -0500

    make schema builder and fix test

--- pyjac/tests/test_schemas.py
+++ pyjac/tests/test_schemas.py
@@ -5,24 +5,22 @@ the given example specifications against them.
 
 """
 
-from os.path import join, isfile
+from os.path import isfile
 import six
-import yamale
 
 # internal
 from ..utils import func_logger
 from .test_utils import xfail
 from ..examples import examples_dir
-from ..schemas import schema_dir, get_validators
+from ..schemas import schema_dir, get_validators, build_schema, validate, __prefixify
 
 
 @func_logger
 def runschema(schema, source, validators=get_validators(),
-              should_fail=False, includes=['common_schema.yaml']):
-    def __prefixify(file, dirname):
-        if dirname not in file:
-            return join(dirname, file)
-        return file
+              should_fail=False, includes=[]):
+
+    # add common
+    includes.append('common_schema.yaml')
 
     # check source / schema / includes, and prepend prefixes
     def __check(file, fdir=schema_dir):
@@ -39,22 +37,8 @@ def runschema(schema, source, validators=get_validators(),
     @xfail(should_fail)
     def _internal(source, schema, validators, includes):
         # make schema
-        schema = yamale.make_schema(schema, validators=validators)
-        # next, go through additional includes and add to schema
-        for inc in includes:
-            # parse other schema
-            inc = yamale.readers.parse_file(inc, 'PyYAML')
-            for i in inc:
-                for k, v in six.iteritems(i):
-                    try:
-                        schema[k]
-                    except KeyError:
-                        # new include
-                        schema.add_include({k: v})
-        # make data
-        source = yamale.make_data(source)
-        # and validate
-        return yamale.validate(schema, source) is not None
+        schema = build_schema(schema, includes=includes)
+        return validate(schema, source) is not None
 
     assert _internal(source, schema, validators, includes)
 
@@ -68,6 +52,5 @@ def test_codegen_platform_schema():
 
 
 def test_mechanism_schema():
-    import pdb; pdb.set_trace()
     runschema('test_matrix_schema.yaml', 'test_matrix.yaml',
               includes=['platform_schema.yaml'])
